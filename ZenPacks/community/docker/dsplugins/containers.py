a20da0111d2889ed290af4e3710c38186f4b7146
hemie143 <hemie143@gmail.com>
2021-11-26 17:37:10 +0100

    20211126_1737

--- ZenPacks/community/docker/dsplugins/containers.py
+++ ZenPacks/community/docker/dsplugins/containers.py
@@ -5,6 +5,8 @@ import re
 
 # Zenoss imports
 from ZenPacks.zenoss.PythonCollector.datasources.PythonDataSource import PythonDataSourcePlugin
+from Products.DataCollector.plugins.DataMaps import ObjectMap, RelationshipMap
+from Products.ZenUtils.Utils import prepId
 
 # Twisted Imports
 from twisted.internet.defer import returnValue, inlineCallbacks
@@ -28,7 +30,7 @@ class stats(PythonDataSourcePlugin):
 
     clients = {}
     commands = {
-        # 'containers': 'sudo docker ps -a --no-trunc',
+        'containers': 'sudo docker ps -a --no-trunc',
         # 'cgroup': 'cat /proc/self/mountinfo | grep cgroup',
         'stats': 'sudo docker stats -a --no-stream --no-trunc',
     }
@@ -135,10 +137,26 @@ class stats(PythonDataSourcePlugin):
         data = self.new_data()
         now = int(time.time())
 
+        ds0 = config.datasources[0]
+        containers_data = ds0.getContainers
+        try:
+            dockerPersistDuration = int(ds0.zDockerPersistDuration)
+        except:
+            dockerPersistDuration = 24
+        time_expiry = now - int(dockerPersistDuration * 3600)
+
+        current_containers = [c.component for c in config.datasources]
+        remaining_instances = list(current_containers)
+        log.debug('XXX current_containers: {}'.format((current_containers)))
+        log.debug('XXX Datasources: {}'.format((config.datasources[0].component)))
+
+        log.debug('XXX Found data for {} current containers'.format(len(ds0.getContainers)))
+
+        containers_maps = []
         if 'containers' in results:
             if results['containers'].exitCode == 0:
-                containers = get_containers(result['containers'].output)
-                log.debug('containers: {}'.format(containers))
+                containers = get_containers(results['containers'].output)
+                log.debug('XXX get_containers: {}'.format(len(containers)))
             else:
                 containers = []
                 log.error('Could not collect containers on {}: (code:{}) {}'.format(config.id,
@@ -149,11 +167,14 @@ class stats(PythonDataSourcePlugin):
             if results['stats'].exitCode == 0:
                 stats_data = get_container_stats(results['stats'].output, log)
                 # log.debug('stats_data: {}'.format(stats_data))
+                stats_data = stats_data[:45]
             else:
                 stats_data = []
                 log.error('Could not collect containers on {}: (code:{}) {}'.format(config.id,
                                                                                     results['stats'].exitCode,
                                                                                     results['stats'].output))
+
+            log.debug('XXX Updating metrics for {} containers'.format(len(stats_data)))
             for container in stats_data:
                 # CONTAINER ID - NAME - CPU % - MEM USAGE / LIMIT - MEM %  - NET I/O - BLOCK I/O - PIDS
                 log.debug('container: {}'.format(container))
@@ -169,27 +190,15 @@ class stats(PythonDataSourcePlugin):
                 data['values'][c_id]['stats_cpu_usage_percent'] = value
 
                 # MEM USAGE / LIMIT
-                mem_metrics = container["MEM USAGE / LIMIT"]
-                log.debug('mem_metrics: **{}**'.format(mem_metrics))
-                r = re.match(r'(\d+\.?\d*)(\w+)\s\/\s(\d+\.?\d*)(\w+)', mem_metrics)
-                log.debug('r: **{}**'.format(r))
-                if r:
-                    usage = convert_from_human(r.group(1), r.group(2))
-                    log.debug('usage: {}'.format(usage))
-                    data['values'][c_id]['stats_memory_usage'] = usage
-                    limit = convert_from_human(r.group(3), r.group(4))
-                    log.debug('limit: {}'.format(limit))
-                    data['values'][c_id]['stats_memory_limit'] = limit
-                else:
-                    data['values'][c_id]['stats_memory_limit'] = 0
-                    data['values'][c_id]['stats_memory_usage'] = 0
+                metric1, metric2 = self.stats_pair(container["MEM USAGE / LIMIT"])
+                data['values'][c_id]['stats_memory_usage'] = metric1
+                data['values'][c_id]['stats_memory_limit'] = metric2
 
                 # MEM %
                 mem_perc = container["MEM %"]
                 r = re.match(r'(\d+\.\d+).*', mem_perc)
                 if r:
                     value = float(r.group(1))
-                log.debug('mem_perc: {}'.format(value))
                 data['values'][c_id]['stats_memory_usage_percent'] = value
 
                 # NET I/O
@@ -207,15 +216,77 @@ class stats(PythonDataSourcePlugin):
                 r = re.match(r'(\d+).*', pids)
                 if r:
                     value = float(r.group(1))
-                log.debug('pids: {}'.format(value))
                 data['values'][c_id]['stats_num_procs'] = value
 
+            for container in stats_data:
+                # log.debug('container: {}'.format(container))
+                c_instance = ObjectMap()
+                instance_id = prepId('container_{}'.format(container["CONTAINER ID"]))
+                c_instance.id = instance_id
+                if instance_id in remaining_instances:
+                    remaining_instances.remove(instance_id)
 
+                c_instance.title = container["NAME"]
+                c_instance.last_seen_model = now
+                # log.debug('c_instance: {}'.format(c_instance))
+                containers_maps.append(c_instance)
+
+        log.debug('XXX Created mapping for {} containers'.format(len(containers_maps)))
         # Build full maps for new containers
 
-        # Check containers that have expired
 
-        # Fill in metrics for remaining containers (not listed but not expired yet)
+        # There must be at least one placeholder instance or the collector won't run. Emptying the list is suicide
+        # containers_maps = []
+        log.debug('XXX Found {} containers'.format(len(containers_maps)))
+        if len(containers_maps) == 0:
+            log.debug('XXX Creating placeholder instance')
+            c_instance = ObjectMap()
+            c_instance.id = 'container_PLACEHOLDER'
+            c_instance.title = 'PLACEHOLDER (Not a real container)'
+            c_instance.container_status = 'EXITED'
+            c_instance.last_seen_model = now
+            log.debug('c_instance: {}'.format(c_instance))
+            containers_maps.append(c_instance)
+
+        # TODO: Remove placeholder if there are containers
+
+        # Review container instances
+        log.debug('XXX Remaining {} container instances'.format(len(remaining_instances)))
+        for container in remaining_instances:
+            if container in containers_data:
+                log.debug('YYY container instance {} found in data'.format(container))
+                log.debug('YYY container data: {}'.format(containers_data[container]))
+                last_seen = max(int(containers_data[container]['collect']), int(containers_data[container]['model']))
+                log.debug('YYY container last seen: {}'.format(containers_data[last_seen]))
+                # Check containers that have expired
+                if last_seen > time_expiry:
+                    c_instance = ObjectMap()
+                    instance_id = prepId('container_{}'.format(instance_id))
+                    c_instance.id = instance_id
+                    containers_maps.append(c_instance)
+                    # Fill in metrics for remaining containers
+                    c_id = 'container_{}'.format(container["CONTAINER ID"])
+                    data['values'][instance_id]['stats_cpu_usage_percent'] = 0
+                    data['values'][instance_id]['stats_cpu_usage_percent'] = 0
+                    data['values'][instance_id]['stats_memory_usage'] = 0
+                    data['values'][instance_id]['stats_memory_limit'] = 0
+                    data['values'][instance_id]['stats_memory_usage_percent'] = 0
+                    data['values'][instance_id]['stats_network_inbound'] = 0
+                    data['values'][instance_id]['stats_network_outbound'] = 0
+                    data['values'][instance_id]['stats_block_read'] = 0
+                    data['values'][instance_id]['stats_block_write'] = 0
+                    data['values'][instance_id]['stats_num_procs'] = 0
+            else:
+                log.debug('YYY container instance {} NOT found in data'.format(container))
+        log.debug('XXX Found {} containers'.format(len(containers_maps)))
+
+
+        data['maps'].append(RelationshipMap(compname='',
+                                            relname='dockerContainers',
+                                            modname='ZenPacks.community.Docker.DockerContainer',
+                                            objmaps=containers_maps,
+                                            ))
+
 
 
         return data
