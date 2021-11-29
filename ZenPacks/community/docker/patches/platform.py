32363828289ec4619a648473561999c9aeea889e
hemie143 <hemie143@gmail.com>
2021-11-29 12:11:11 +0100

    test

--- ZenPacks/community/docker/patches/platform.py
+++ ZenPacks/community/docker/patches/platform.py
@@ -4,3 +4,8 @@ from Products.ZenUtils.Utils import monkeypatch
 @monkeypatch('Products.ZenModel.Device.Device')
 def getContainers(self):
     return {c.id: {'model':c.last_seen_model, 'collect':c.last_seen_collect()} for c in self.dockerContainers()}
+
+def getContainers_lastSeen(self):
+    return {c.id: c.last_seen_model for c in self.dockerContainers()}
+
+# This is a test
